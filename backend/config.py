dce7b97c71a832b537d87d7294ba697eec3a84dd
Alexander2817 <alande10@charlotte.edu>
2025-09-16 22:55:20 -0400

    feat:Added backend structure

---
+++ backend/config.py
@@ -0,0 +1,8 @@
+class conf:
+    db_host = "localhost"
+    db_name = "final_project"
+    db_port = 3306
+    db_user = "root"
+    db_password = "root"
+    app_host = "localhost"
+    app_port = 8009
